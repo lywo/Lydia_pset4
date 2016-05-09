e0dce453548c34afbbcf07ba42ca5812a308cfde
lywo <lydia.wolfs@student.uva.nl>
2016-05-09 12:31:11 +0200

    Day 1

--- app/src/main/java/com/example/lydia/lydia_pset4/SecondActivity.java
+++ app/src/main/java/com/example/lydia/lydia_pset4/SecondActivity.java
@@ -1,11 +1,8 @@
 package com.example.lydia.lydia_pset4;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.widget.ListView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,17 +10,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ListView todoItemsLV = (ListView) findViewById(R.id.todoItemsLV);
     }
-
 }
